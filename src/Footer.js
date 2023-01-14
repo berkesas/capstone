381d54cf6dc85989f5898e995224dba76be9c369
berkesas <nazarmammedov2016@gmail.com>
2023-01-14 17:23:41 +0500

    meta opengraph

---
+++ src/Footer.js
@@ -0,0 +1,33 @@
+function Footer() {
+    return (
+        <footer>
+            <nav>
+                <h4>Sitemap</h4>
+                <ul>
+                    <li><a href='/home'>Home</a></li>
+                    <li><a href='/about'>About</a></li>
+                    <li><a href='/menu'>Menu</a></li>
+                    <li><a href='/reservations'>Reservations</a></li>
+                    <li><a href='/order-online'>Order online</a></li>
+                    <li><a href='/login'>Login</a></li>
+                </ul>
+            </nav>
+            <div>
+                <h4>Contact</h4>
+                Hope Street 54, Chicago, IL, USA
+                +1 809 908 98
+                contact@littlelemon.com
+            </div>
+            <div>
+                <h4>Social media links</h4>
+                <ul>
+                    <li><a href='https://facebook.com'>Facebook</a></li>
+                    <li><a href='https://youtube.com'>Youtube</a></li>
+                    <li><a href='https://instagram.com'>Instagram</a></li>
+                </ul>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import logo from './assets/img/logo.svg';
+import Nav from './Nav';
+
+function Header() {
+  return (
+    <header>
+      <div>
+        <img src={logo} alt='logo' />
+      </div>
+      <Nav />
+    </header>
+  );
+}
+
+export default Header;
